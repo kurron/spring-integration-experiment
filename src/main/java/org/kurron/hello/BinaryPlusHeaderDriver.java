6bd7935f5660f4a8ab5c6abbd361f6447a0b0647
Ronald D. Kurr <kurr@kurron.org>
2013-03-27 16:48:23 -0400

    Added the start of another example.

---
+++ src/main/java/org/kurron/hello/BinaryPlusHeaderDriver.java
@@ -0,0 +1,23 @@
+package org.kurron.hello;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ron
+ * Date: 3/22/13
+ * Time: 8:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BinaryPlusHeaderDriver
+{
+
+    public static void main( final String[] args ) throws InterruptedException
+    {
+        final AbstractApplicationContext context = new ClassPathXmlApplicationContext( "/META-INF/spring/integration/binary-plus-header-context.xml", BinaryPlusHeaderDriver.class );
+        final Publisher bean = context.getBean( "automatedPublisher", Publisher.class );
+        bean.go();
+        context.registerShutdownHook();
+    }
+}
